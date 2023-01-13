1649343784b19029f30c928842103a685c95ff26
ofuta <ofutaru@proton.me>
2023-01-14 00:13:34 +0300

    test

--- setup-git-v5.sh
+++ setup-git-v5.sh
@@ -57,7 +57,7 @@ echo
 project=$(basename `pwd`)
 #githubdir=$(basename $(dirname "$PWD" | tr '[:upper:]' '[:lower:]'))
 echo "-----------------------------------------------------------------------------"
-echo "this is project https://github.com/ofuta/"$project
+echo "this is project https://github.com/erikdubois/"$project
 echo "-----------------------------------------------------------------------------"
 
 git config --global pull.rebase false
@@ -68,7 +68,7 @@ sudo git config --system core.editor nano
 #git config --global credential.helper cache
 #git config --global credential.helper 'cache --timeout=32000'
 
-git remote set-url origin github.com:ofuta/$project
+git remote set-url origin git@github.com:ofuta/$project
 
 echo
 tput setaf 3
